c41c5783e67e7811f9ec14036632148124e37787
Pinto <pintokevin2002@hotmail.com>
2025-02-18 13:47:31 +0100

    Adding the layout

---
+++ src/components/Layout.jsx
@@ -0,0 +1,14 @@
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+
+const Layout = () => {
+    return (
+        <div className="App grid-rows-app">
+            <Header />
+            <div>lol</div>
+            <Footer />
+        </div>
+    );
+};
+
+export default Layout;
