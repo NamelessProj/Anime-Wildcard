70bd9fd649059859604210991400c2bfb2c1631b
Pinto <pintokevin2002@hotmail.com>
2025-02-18 13:41:47 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+
+const App = () => {
+
+    return (
+        <div className="App grid-rows-app">
+            <Header />
+            <div>lol</div>
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      gridTemplateRows: {
+        'app': 'auto 1fr auto',
+      }
+    },
+    textWrap: {
+      'balance': 'balance',
+    },
+    colors: {
+      'primary': {
+        0: '#171717',
+        300: '#fddc75',
+        400: '#bc4b27',
+        900: '#d9d9d9',
+        999: '#ffffff',
+        'blue': {
+          'dark': '#101720',
+        },
+      }
+    }
+  },
+  plugins: [],
+});
