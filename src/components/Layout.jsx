6353d60bbf6c0195c863b49f2454201185c84b91
Kevin <pintokevin2002@hotmail.com>
2025-02-19 21:06:23 +0100

    Updating the Layout to use the Outlet component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,12 @@
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
+import {Outlet} from "react-router-dom";
 
 const Layout = () => {
     return (
         <div className="App grid-rows-app">
             <Header />
-            <div>lol</div>
+            <Outlet />
             <Footer />
         </div>
     );
