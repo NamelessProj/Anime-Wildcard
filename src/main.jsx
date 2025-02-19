b8acecd468bdff204c15118a04011227b1a67829
Kevin <pintokevin2002@hotmail.com>
2025-02-19 21:04:59 +0100

    Updating the main

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import {DataProvider} from "./context/DataContext.jsx";
 import App from './App.jsx';
 import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
+import {ThemeProvider} from "@material-tailwind/react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 const client = new ApolloClient({
     uri: "https://graphql.anilist.co/",
@@ -11,8 +14,18 @@ const client = new ApolloClient({
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <ApolloProvider client={client}>
-            <App />
-        </ApolloProvider>
+        <DataProvider>
+            <ThemeProvider>
+                <ApolloProvider client={client}>
+                    <BrowserRouter future={{
+                        v7_startTransition: true,
+                    }}>
+                        <Routes>
+                            <Route path="/*" element={<App />} />
+                        </Routes>
+                    </BrowserRouter>
+                </ApolloProvider>
+            </ThemeProvider>
+        </DataProvider>
     </StrictMode>,
 );
