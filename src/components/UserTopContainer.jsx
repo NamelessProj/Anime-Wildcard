0f00454dbd5df2259c96681bf31863f8cce3bd4a
Kevin <pintokevin2002@hotmail.com>
2025-02-20 17:51:08 +0100

    Adding an error message if the user doesn't have enough anime in his list

--- src/components/UserTopContainer.jsx
+++ src/components/UserTopContainer.jsx
@@ -1,13 +1,21 @@
 import {useEffect, useState} from "react";
 import DefaultSpinner from "./DefaultSpinner.jsx";
 import {shuffleArray} from "../utils/shuffleArray.js";
+import {Alert} from "@material-tailwind/react";
 
 const UserTopContainer = ({data}) => {
     const [animeList, setAnimeList] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState("");
 
     useEffect(() => {
         if(data && data.MediaListCollection && data.MediaListCollection.lists){
+            if(data.MediaListCollection.lists.length < 5){
+                setError("You need to have at least 5 anime in your list.");
+                setIsLoading(false);
+                return;
+            }
+
             const list = data.MediaListCollection.lists.flatMap(list => list.entries);
             const shuffledList = shuffleArray(list);
 
@@ -23,14 +31,24 @@ const UserTopContainer = ({data}) => {
                     <DefaultSpinner />
                 </div>
             ) : (
-                <div>
-                    {animeList.map((anime) => (
-                        <div key={anime.id}>
-                            <img src={anime.media.coverImage.large} alt={anime.media.title.romaji} />
-                            <h3>{anime.media.title.romaji}</h3>
+                <>
+                    {error ? (
+                        <div className="h-full flex justify-center items-center">
+                            <Alert color="red">
+                                {error}
+                            </Alert>
                         </div>
-                    ))}
-                </div>
+                    ) : (
+                        <div>
+                            {animeList.map((anime) => (
+                                <div key={anime.id}>
+                                    <img src={anime.media.coverImage.large} alt={anime.media.title.romaji} />
+                                    <h3>{anime.media.title.romaji}</h3>
+                                </div>
+                            ))}
+                        </div>
+                    )}
+                </>
             )}
         </>
     );
