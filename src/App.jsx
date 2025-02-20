15d1d600877ca962888db04e5fcbd43012c67db3
Kevin <pintokevin2002@hotmail.com>
2025-02-20 17:43:08 +0100

    Adding a route to get to the TopPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import {Routes, Route} from "react-router-dom";
 import Layout from "./components/Layout.jsx";
 import Home from "./pages/Home.jsx";
+import TopPage from "./pages/TopPage.jsx";
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<Layout />}>
                 <Route index element={<Home />} />
+                <Route path="top" element={<TopPage />} />
             </Route>
         </Routes>
     );
