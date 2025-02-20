7efc4cb5c3e5504359bf602d3f1a02a026d70b62
Kevin <pintokevin2002@hotmail.com>
2025-02-20 17:03:35 +0100

    Updating the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,21 @@
+import {Typography} from "@material-tailwind/react";
+import {FaGithub} from "react-icons/fa";
+
 const Header = () => {
     return (
-        <header>
+        <header className="p-3">
+            <div className="flex justify-between items-center">
+                <Typography variant="h2" as="p">
+                    Your BLinded Anime Top
+                </Typography>
 
+                <a target="_blank" href="https://github.com/NamelessProj/Your_Blinded_Anime_Top">
+                    <Typography className="sr-only">
+                        View on GitHub
+                    </Typography>
+                    <FaGithub size={24} />
+                </a>
+            </div>
         </header>
     );
 };
