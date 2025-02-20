83b740a73754def0d242716a64cb21e0f963603c
Kevin <pintokevin2002@hotmail.com>
2025-02-20 18:20:21 +0100

    Updating the way data are displayed

--- src/components/UserTopContainer.jsx
+++ src/components/UserTopContainer.jsx
@@ -1,23 +1,28 @@
-import {useEffect, useState} from "react";
+import {useContext, useEffect, useState} from "react";
 import DefaultSpinner from "./DefaultSpinner.jsx";
 import {shuffleArray} from "../utils/shuffleArray.js";
-import {Alert} from "@material-tailwind/react";
+import {Alert, Button} from "@material-tailwind/react";
+import DataContext from "../context/DataContext.jsx";
+import {Link} from "react-router-dom";
 
 const UserTopContainer = ({data}) => {
+    const {getAdult} = useContext(DataContext);
+
     const [animeList, setAnimeList] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState("");
 
     useEffect(() => {
         if(data && data.MediaListCollection && data.MediaListCollection.lists){
-            if(data.MediaListCollection.lists.length < 5){
-                setError("You need to have at least 5 anime in your list.");
+            const list = data.MediaListCollection.lists.flatMap(list => list.entries);
+
+            const shuffledList = shuffleArray(getAdult ? list : list.filter(entry => !entry.media.isAdult));
+
+            if(shuffledList.length < 5){
+                setError("You need at least 5 anime in your list to generate a top.");
                 setIsLoading(false);
                 return;
             }
-
-            const list = data.MediaListCollection.lists.flatMap(list => list.entries);
-            const shuffledList = shuffleArray(list);
 
             setAnimeList(shuffledList.slice(0, 5));
             setIsLoading(false);
@@ -31,10 +36,20 @@ const UserTopContainer = ({data}) => {
                     <DefaultSpinner />
                 </div>
             ) : (
-                <>
+                <div>
+                    <div className="flex justify-center my-2">
+                        <Button
+                            color="deep-orange"
+                            variant="gradient"
+                        >
+                            <Link to="/">
+                                Go Back
+                            </Link>
+                        </Button>
+                    </div>
                     {error ? (
-                        <div className="h-full flex justify-center items-center">
-                            <Alert color="red">
+                        <div className="flex justify-center my-2">
+                            <Alert color="red" className="w-fit">
                                 {error}
                             </Alert>
                         </div>
@@ -48,7 +63,7 @@ const UserTopContainer = ({data}) => {
                             ))}
                         </div>
                     )}
-                </>
+                </div>
             )}
         </>
     );
