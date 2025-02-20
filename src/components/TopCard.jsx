7ed77cdc30033c2b3590ea7450457fb774a89a16
Kevin <pintokevin2002@hotmail.com>
2025-02-20 21:51:10 +0100

    Using the library react-card-flip

--- src/components/TopCard.jsx
+++ src/components/TopCard.jsx
@@ -1,24 +1,51 @@
 import {Tooltip, Typography} from "@material-tailwind/react";
+import {useEffect, useState} from "react";
+import ReactCardFlip from "react-card-flip";
 
-const TopCard = ({anime, handler}) => {
+const TopCard = ({index, anime, handler}) => {
+    const [isFlipped, setIsFlipped] = useState(false);
+    const [name, setName] = useState(index + 1);
+
+    useEffect(() => {
+        if(anime){
+            setIsFlipped(true);
+            setName(anime.media.title.english || anime.media.title.romaji);
+        }
+    }, [anime]);
+
+    const imgProps = {
+        className: "w-full h-full rounded-[5px] overflow-clip object-cover",
+    };
+
+    const typoProps = {
+        className: "text-nowrap whitespace-nowrap overflow-clip",
+        style: {textOverflow: "ellipsis"},
+    }
+
     return (
         <button onClick={handler} className="w-24">
-            <div className="aspect-[9/16] h-40 rounded-md overflow-clip">
-                <img src={anime ? anime.media.coverImage.large : './card_back.webp'} alt={anime ? anime.media.title.romaji : ''} />
+            <div className="mb-2">
+                <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal" containerClassName="w-24 h-36">
+                    <img {...imgProps} src="./card_back.webp" alt="" />
+                    <img {...imgProps} src={anime ? anime.media.coverImage.large : "./card_back.webp"} alt={name} />
+                </ReactCardFlip>
             </div>
-            {anime &&
-                <Tooltip content={anime.media.title.english || anime.media.title.romaji}>
+            <Tooltip content={name}>
+                {typeof name === "string" ?
                     <Typography
-                        className="text-nowrap whitespace-nowrap overflow-clip"
-                        style={{textOverflow: "ellipsis"}}
+                        {...typoProps}
                         as="a"
                         href={`https://anilist.co/anime/${anime.media.id}`}
                         target="_blank"
                     >
-                        {anime.media.title.english || anime.media.title.romaji}
+                        {name}
                     </Typography>
-                </Tooltip>
-            }
+                    :
+                    <Typography {...typoProps}>
+                        {name}
+                    </Typography>
+                }
+            </Tooltip>
         </button>
     );
 };
