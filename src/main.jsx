4d606d35656b6c815a2b763bc69854f590eb230a
Kevin <pintokevin2002@hotmail.com>
2025-02-20 15:36:23 +0100

    Importing the style first

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
+import './index.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import {DataProvider} from "./context/DataContext.jsx";
 import App from './App.jsx';
 import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
