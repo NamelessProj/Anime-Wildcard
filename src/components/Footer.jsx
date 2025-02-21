98d487b1f04a5169e019357ce2ba33aa45ed3ce5
Kevin <pintokevin2002@hotmail.com>
2025-02-21 16:58:03 +0100

    Making the copyright more visible

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer className="p-1">
             <Typography variant="small" className="text-center text-balance mx-auto">
-                &copy; {format(new Date(), "MMM yyyy")} - All rights reserved - <a target="_blank" href="https://github.com/NamelessProj">Da Silva Pinto Kevin</a>
+                &copy; {format(new Date(), "MMM yyyy")} - All rights reserved - <a className="text-primary-400 opacity-80 hover:opacity-100" target="_blank" href="https://github.com/NamelessProj">Da Silva Pinto Kevin</a>
             </Typography>
         </footer>
     );
