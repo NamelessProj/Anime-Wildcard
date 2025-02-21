3abe2131aa8d305e4ccfd33a20e49fb9aa60c1de
Kevin <pintokevin2002@hotmail.com>
2025-02-21 16:29:51 +0100

    Adding a way to tell when a show is for adult

--- src/components/AnimeCard.jsx
+++ src/components/AnimeCard.jsx
@@ -2,6 +2,7 @@ import ReactCardFlip from "react-card-flip";
 import {useState} from "react";
 import {Button, Tooltip, Typography} from "@material-tailwind/react";
 import {FaExternalLinkAlt} from "react-icons/fa";
+import {TbRating18Plus} from "react-icons/tb";
 
 const AnimeCard = ({anime}) => {
     const [isFlipped, setIsFlipped] = useState(false);
@@ -43,6 +44,11 @@ const AnimeCard = ({anime}) => {
                         >
                             <FaExternalLinkAlt size={15} />
                         </Button>
+                        {anime.isAdult &&
+                            <div className="absolute left-0 top-0">
+                                <TbRating18Plus size={36} color="red" />
+                            </div>
+                        }
                         <div className="absolute left-0 right-0 bottom-0 p-2 bg-gray-800 bg-opacity-70 backdrop-blur-sm z-10">
                             <Typography variant="lead" className="text-nowrap whitespace-nowrap overflow-clip" style={{textOverflow: "ellipsis"}}>
                                 {name}
