003e8ced8daa2410d77ca9db8c3be5175c4386ff
Kevin <pintokevin2002@hotmail.com>
2025-02-22 11:26:12 +0100

    Adding the ErrorPage to the router

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {Routes, Route} from "react-router-dom";
 import Layout from "./components/Layout.jsx";
 import Home from "./pages/Home.jsx";
 import TopPage from "./pages/TopPage.jsx";
+import ErrorPage from "./pages/ErrorPage.jsx";
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <Route path="/" element={<Layout />}>
                 <Route index element={<Home />} />
                 <Route path="top" element={<TopPage />} />
+                <Route path="*" element={<ErrorPage />} />
             </Route>
         </Routes>
     );
